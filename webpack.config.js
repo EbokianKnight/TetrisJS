ffda33e4c1388883283b6ac6d9b2dbd6aeb2238e
Adam <ebokianknight@gmail.com>
2016-04-13 08:29:24 -0400

    Game Setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/setup.js",
+  output: {
+    path: "./frontend/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  }
+};
